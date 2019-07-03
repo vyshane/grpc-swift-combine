19f419aaa9f7faf229b7b1b028cb478e63c63779
Vy-Shane Xie <shane@node.mu>
2019-07-03 22:37:59 +0800

    Add grpc-swift dependency

--- Package.swift
+++ Package.swift
@@ -12,15 +12,14 @@ let package = Package(
             targets: ["CombineGRPC"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/grpc/grpc-swift.git", from: "1.0.0-alpha.1"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "CombineGRPC",
-            dependencies: []),
+            dependencies: ["GRPC"]),
         .testTarget(
             name: "CombineGRPCTests",
             dependencies: ["CombineGRPC"]),
