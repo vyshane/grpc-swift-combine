cd38a8c34718f856d42c93a4f62a87479402a467
Vy-Shane Xie <shane@node.mu>
2019-07-04 22:08:03 +0800

    Add type aliases for remaining RPC types

--- Sources/CombineGRPC/CombineGRPC.swift
+++ Sources/CombineGRPC/CombineGRPC.swift
@@ -2,16 +2,35 @@ import Combine
 import GRPC
 import SwiftProtobuf
 
-public typealias UnaryRPC<A, B> = (A, CallOptions?) -> UnaryCall<A, B> where A: Message, B: Message
+public typealias UnaryRPC<A, B> = (A, CallOptions?) -> UnaryCall<A, B>
+  where A: Message, B: Message
 
-public func call<A, B>(_ rpc: @escaping UnaryRPC<A, B>) -> (A, CallOptions) -> UnaryCallPublisher<A, B> where A: Message, B: Message {
-  return { request, callOptions in
-    UnaryCallPublisher(rpc(request, callOptions))
+public typealias ServerStreamingRPC<A, B> = (A, CallOptions?, (B) -> Void) -> ServerStreamingCall<A, B>
+  where A: Message, B: Message
+
+public typealias ClientStreamRPC<A, B> = (CallOptions?) -> ClientStreamingCall<A, B>
+  where A: Message, B: Message
+
+public typealias BidirectionalStreamRPC<A, B> = (CallOptions?, (B) -> Void) -> BidirectionalStreamingCall<A, B>
+  where A: Message, B: Message
+
+
+public func call<A, B>(_ rpc: @escaping UnaryRPC<A, B>)
+  -> (A)
+  -> UnaryCallPublisher<A, B>
+  where A: Message, B: Message
+{
+  return { request in
+    UnaryCallPublisher(rpc(request, nil))
   }
 }
 
-public func call<A, B>(_ rpc: @escaping UnaryRPC<A, B>) -> (A) -> UnaryCallPublisher<A, B> where A: Message, B: Message {
-  return { request in
-    UnaryCallPublisher(rpc(request, nil))
+public func call<A, B>(_ rpc: @escaping UnaryRPC<A, B>)
+  -> (A, CallOptions)
+  -> UnaryCallPublisher<A, B>
+  where A: Message, B: Message
+{
+  return { request, callOptions in
+    UnaryCallPublisher(rpc(request, callOptions))
   }
 }
