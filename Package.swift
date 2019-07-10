b1f9bd4a77af20f6b5064df312a81cbdbfe3c1d8
Vy-Shane Xie <shane@node.mu>
2019-07-10 22:03:20 +0800

    Add platforms to SPM config

--- Package.swift
+++ Package.swift
@@ -5,6 +5,10 @@ import PackageDescription
 
 let package = Package(
     name: "CombineGRPC",
+    platforms: [
+        .macOS(.v10_14),
+        .iOS(.v12),
+    ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
