999d68bb674fe2214f6c764c5d10e9e89ab03b0f
Vy-Shane Xie <shane@node.mu>
2019-07-11 23:31:14 +0800

    Implement unary handler subscriber

--- Sources/CombineGRPC/Server/Handlers.swift
+++ Sources/CombineGRPC/Server/Handlers.swift
@@ -12,25 +12,9 @@ import SwiftProtobuf
 public func handle<Response>(_ context: StatusOnlyCallContext,
                              handler: () -> AnyPublisher<Response, GRPCStatus>) -> EventLoopFuture<Response>
 {
-  // TODO: Abstract out shared implementation
-  var future: EventLoopFuture<Response>?
-  _ = handler().sink(
-    receiveCompletion: { completion in
-      switch completion {
-      case .failure(let status):
-        future = context.eventLoop.makeFailedFuture(status)
-      case .finished:
-        let status = GRPCStatus(code: .aborted,
-                                message: "Response publisher completed without sending a value")
-        future = context.eventLoop.makeFailedFuture(status)
-      }
-    },
-    receiveValue: { response in
-      // First value received will complete the call
-      future = context.eventLoop.makeSucceededFuture(response)
-    }
-  )
-  return future!
+  let unarySubscriber = UnaryHandlerSubscriber<Response>(context: context)
+  _ = handler().subscribe(unarySubscriber)
+  return unarySubscriber.promise.futureResult
 }
 
 @available(OSX 10.15, *)
@@ -38,23 +22,7 @@ public func handle<Request, Response>(_ request: Request, _ context: StatusOnlyC
                                       handler: (Request) -> AnyPublisher<Response, GRPCStatus>)
                                      -> EventLoopFuture<Response>
 {
-  // TODO: Abstract out shared implementation
-  var future: EventLoopFuture<Response>?
-  _ = handler(request).sink(
-    receiveCompletion: { completion in
-      switch completion {
-      case .failure(let status):
-        future = context.eventLoop.makeFailedFuture(status)
-      case .finished:
-        let status = GRPCStatus(code: .aborted,
-                                message: "Response publisher completed without sending a value")
-        future = context.eventLoop.makeFailedFuture(status)
-      }
-    },
-    receiveValue: { response in
-      // First value received will complete the call
-      future = context.eventLoop.makeSucceededFuture(response)
-    }
-  )
-  return future!
+  let unarySubscriber = UnaryHandlerSubscriber<Response>(context: context)
+  _ = handler(request).subscribe(unarySubscriber)
+  return unarySubscriber.promise.futureResult
 }
--- Sources/CombineGRPC/Server/UnaryHandlerSubscriber.swift
+++ Sources/CombineGRPC/Server/UnaryHandlerSubscriber.swift
@@ -4,5 +4,42 @@
 import Foundation
 import Combine
 import GRPC
+import NIO
 import SwiftProtobuf
 
+@available(OSX 10.15, *)
+class UnaryHandlerSubscriber<Response>: Subscriber, Cancellable {
+  typealias Input = Response
+  typealias Failure = GRPCStatus
+  
+  let promise: EventLoopPromise<Response>
+  var subscription: Subscription?
+  
+  init(context: StatusOnlyCallContext) {
+    self.promise = context.eventLoop.makePromise()
+  }
+  
+  func receive(subscription: Subscription) {
+    self.subscription = subscription
+    self.subscription?.request(.unlimited)
+  }
+  
+  func receive(_ input: Response) -> Subscribers.Demand {
+    promise.succeed(input)
+    return .unlimited
+  }
+  
+  func receive(completion: Subscribers.Completion<GRPCStatus>) {
+    switch completion {
+    case .failure(let status):
+      promise.fail(status)
+    case .finished:
+      let status = GRPCStatus(code: .aborted, message: "Response publisher completed without sending a value")
+      promise.fail(status)
+    }
+  }
+  
+  func cancel() {
+    subscription?.cancel()
+  }
+}
