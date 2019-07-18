577ebc024be4b63aa6e4343c9f0fa0c5dcf1e261
Vy-Shane Xie <shane@node.mu>
2019-07-18 21:58:55 +0800

    Add client streaming test stubs

--- Tests/CombineGRPCTests/ClientStreamingTests.swift
+++ Tests/CombineGRPCTests/ClientStreamingTests.swift
@@ -8,4 +8,39 @@ import NIO
 @testable import CombineGRPC
 
 class ClientStreamingTests: XCTestCase {
+  
+  static var serverEventLoopGroup: EventLoopGroup?
+  static var client: UnaryScenariosServiceClient?
+  
+  override class func setUp() {
+    super.setUp()
+    serverEventLoopGroup = try! makeTestServer(services: [UnaryTestsService()])
+    client = makeTestClient { connection, callOptions in
+      UnaryScenariosServiceClient(connection: connection, defaultCallOptions: callOptions)
+    }
+  }
+  
+  override class func tearDown() {
+    try! client?.connection.close().wait()
+    try! serverEventLoopGroup?.syncShutdownGracefully()
+    super.tearDown()
+  }
+  
+  func testClientStreamOk() {
+    XCTFail("TODO")
+  }
+  
+  func testClientStreamFailedPrecondition() {
+    XCTFail("TODO")
+  }
+  
+  func testTestClientStreamNoResponse() {
+    XCTFail("TODO")
+  }
+  
+  static var allTests = [
+    ("Client stream OK", testClientStreamOk),
+    ("Client stream failed precondition", testClientStreamFailedPrecondition),
+    ("Client stream no response", testTestClientStreamNoResponse),
+  ]
 }
---
+++ Tests/CombineGRPCTests/Server Implementations/ClientStreamingTestsService.swift
@@ -0,0 +1,8 @@
+//
+//  ClientStreamingTestsService.swift
+//  CombineGRPCTests
+//
+//  Created by Vy-Shane Xie on 18/7/19.
+//
+
+import Foundation
