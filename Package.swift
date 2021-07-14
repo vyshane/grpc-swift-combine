9715f3ac6aeef15df8d8c5232ce135e1e0e0bdf7
Vy-Shane Xie <s@vyshane.com>
2021-07-14 18:16:20 +0800

    Update upstream grpc-swift to v1.3.0

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
             targets: ["CombineGRPC"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/grpc/grpc-swift.git", .exact("1.2.0")),
+        .package(url: "https://github.com/grpc/grpc-swift.git", .exact("1.3.0")),
         .package(url: "https://github.com/CombineCommunity/CombineExt.git", from: "1.3.0"),
     ],
     targets: [
